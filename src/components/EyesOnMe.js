ccc936b5c5088c87be73a01a7bd8e8939c0a2bdd
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-02-23 13:43:49 -0500

    commit tree 6c9c958643f22d2a859367932030568f2ff70074

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function handleFocus() {
+    console.log('Good!');
+}
+function handleBlur() {
+    console.log('Hey! Eyes on me!');
+}
+
+function EyesOnMe() {
+    return(
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    function handlePassword() {
+        console.log('Entering password...')
+    }
+
+    return (
+        <input type="password" onChange={handlePassword}/>
+    )
+}
+
+export default Keypad;
